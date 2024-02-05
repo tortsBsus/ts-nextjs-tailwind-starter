bb45fae1711fab20fcee1c6100c478924c507edf
Meghana Rathanraj <meganraj251@gmail.com>
2024-02-05 13:25:33 +0530

    Update Intro.tsx

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -3,12 +3,13 @@ import React from 'react'
 function Intro() {
   return (
     <div className="m-10 text-center shadow-xl">
-      <h1>Created by Meghana Rathanraj</h1>
+      <h1>Created by Meghana</h1>
       <p>Thanks for the oppurtunity, it was an interesting assignment!</p>
       <div className='flex justify-evenly m-3 p-10'>
-        <button className="p-4 bg-black text-white rounded-lg" onClick={() => window.location.href = "https://drive.google.com/file/d/1l3g_2eIfqyKRSSK5MpJksebWXPXMkMDZ/view?usp=drive_link"}>Resume</button>
+        <button className="p-4 bg-black text-white rounded-lg" onClick={() => window.location.href = "https://drive.google.com/file/d/1umQavr0K76dStCO3IpvyDTKNlv_mbjaJ/view?usp=sharing"}>Resume</button>
         <button className="p-4 bg-black text-white rounded-lg" onClick={() => window.location.href = "https://www.linkedin.com/in/meghana-rathanraj-link-to-profile/"}>Linkedin</button>
         <button className="p-4 bg-black text-white rounded-lg" onClick={() => window.location.href = "https://twitter.com/megTries"}>Twitter</button>
+        <button className="p-4 bg-black text-white rounded-lg" onClick={() => window.location.href = "https://github.com/tortsBsus/zepto-chip-component-assignment"}>Code</button>
       </div>
     </div>
   )
